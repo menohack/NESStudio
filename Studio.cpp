591a266eaceb85ab3236a23292c2c21d9f5cb435
James Doverspike <menohack@gmail.com>
2013-08-06 21:03:12 -0400

    Fixed formatting

--- Studio.cpp
+++ Studio.cpp
@@ -3,92 +3,88 @@
 using namespace NESStudio;
 
 BEGIN_EVENT_TABLE(Frame, wxFrame)
-    EVT_MENU(ID_Quit,  Frame::OnQuit)
+    EVT_MENU(ID_Quit, Frame::OnQuit)
     EVT_MENU(ID_About, Frame::OnAbout)
     EVT_MENU(ID_Save, Frame::OnSave)
     EVT_MENU(ID_Load, Frame::OnLoad)
-    EVT_BUTTON ( BUTTON_Play, Studio::Play)
+    EVT_BUTTON(BUTTON_Play, Studio::Play)
 END_EVENT_TABLE()
-
 
 void MakeSineWave(Int16 * samples, int numSamples, int rate)
 {
-	for (int i=0; i < numSamples; i++)
-		samples[i] = 32768 * sin(((double)i/rate) * 2.0 * M_PI);
+	for (int i = 0; i < numSamples; i++)
+		samples[i] = 32768 * sin(((double)i / rate) * 2.0 * M_PI);
 }
-
 
 bool Studio::OnInit()
 {
-    frame = new Frame( _("NESStudio"), wxPoint(50, 50),
-                                  wxSize(800,600) );
+	frame = new Frame(_("NESStudio"), wxPoint(50, 50), wxSize(800, 600));
 
-    frame->Show(true);
-    SetTopWindow(frame);
+	frame->Show(true);
+	SetTopWindow(frame);
 
-    int numSamples = 32*1024;
-    samples = new Int16[numSamples];
-    MakeSineWave(samples, numSamples, 32);
-    bool result = buffer.LoadFromSamples(samples, numSamples, 2, numSamples);
+	int numSamples = 32 * 1024;
+	samples = new Int16[numSamples];
+	MakeSineWave(samples, numSamples, 32);
+	bool result =
+	    buffer.LoadFromSamples(samples, numSamples, 2, numSamples);
 
-    return true;
-} 
-
-Frame::Frame(const wxString& title, const wxPoint& pos, const wxSize& size)
-: wxFrame( NULL, -1, title, pos, size)
-{
-    wxMenu *menuFile = new wxMenu;
-
-    menuFile->Append( ID_About, _("&About...") );
-    menuFile->AppendSeparator();
-    menuFile->Append( ID_Save, _("&Save"));
-    menuFile->Append( ID_Save, _("&Load"));
-    menuFile->AppendSeparator();
-    menuFile->Append( ID_Quit, _("E&xit") );
-
-    wxMenuBar *menuBar = new wxMenuBar;
-    menuBar->Append( menuFile, _("&File") );
-
-    wxPanel *panel = new wxPanel(this, wxID_ANY);
-
-    play = new wxButton(this, BUTTON_Play, _("Play"), wxPoint(20,20));
-	
-
-
-    SetMenuBar( menuBar );
-    SetIcon(wxIcon(_("icon.xpm")));
-
-    CreateStatusBar();
-    SetStatusText( _("Welcome to NES Studio!") );
+	return true;
 }
 
-void Frame::OnQuit(wxCommandEvent& WXUNUSED(event))
+ Frame::Frame(const wxString & title, const wxPoint & pos, const wxSize & size):wxFrame(NULL, -1, title, pos,
+	size)
 {
-    Close(TRUE);
+	wxMenu *menuFile = new wxMenu;
+
+	menuFile->Append(ID_About, _("&About..."));
+	menuFile->AppendSeparator();
+	menuFile->Append(ID_Save, _("&Save"));
+	menuFile->Append(ID_Save, _("&Load"));
+	menuFile->AppendSeparator();
+	menuFile->Append(ID_Quit, _("E&xit"));
+
+	wxMenuBar *menuBar = new wxMenuBar;
+	menuBar->Append(menuFile, _("&File"));
+
+	wxPanel *panel = new wxPanel(this, wxID_ANY);
+
+	play = new wxButton(this, BUTTON_Play, _("Play"), wxPoint(20, 20));
+
+	SetMenuBar(menuBar);
+	SetIcon(wxIcon(_("icon.xpm")));
+
+	CreateStatusBar();
+	SetStatusText(_("Welcome to NES Studio!"));
 }
 
-void Frame::OnAbout(wxCommandEvent& WXUNUSED(event))
+void Frame::OnQuit(wxCommandEvent & WXUNUSED(event))
 {
-    wxMessageBox( _("NESStudio copyright 2013 James Doverspike"),
-                  _("About NESStudio"),
-                  wxOK | wxICON_INFORMATION, this);
-}
-void Frame::OnSave(wxCommandEvent& WXUNUSED(event))
-{
-    SetStatusText( _("Saved"));
+	Close(TRUE);
 }
 
-void Frame::OnLoad(wxCommandEvent& WXUNUSED(event))
+void Frame::OnAbout(wxCommandEvent & WXUNUSED(event))
 {
-    SetStatusText( _("Loaded"));
+	wxMessageBox(_("NESStudio copyright 2013 James Doverspike"),
+		     _("About NESStudio"), wxOK | wxICON_INFORMATION, this);
 }
 
-void Studio::Play(wxCommandEvent& WXUNUSED(event))
+void Frame::OnSave(wxCommandEvent & WXUNUSED(event))
 {
-    frame->SetStatusText( _("Play"));
+	SetStatusText(_("Saved"));
+}
 
-    const Int16 * s = buffer.GetSamples();
+void Frame::OnLoad(wxCommandEvent & WXUNUSED(event))
+{
+	SetStatusText(_("Loaded"));
+}
 
-    Sound sound(buffer);
-    sound.Play();
+void Studio::Play(wxCommandEvent & WXUNUSED(event))
+{
+	frame->SetStatusText(_("Play"));
+
+	const Int16 *s = buffer.GetSamples();
+
+	Sound sound(buffer);
+	sound.Play();
 }
--- Studio.h
+++ Studio.h
@@ -1,51 +1,46 @@
-#include "wx/wx.h" 
+#include "wx/wx.h"
 #include <SFML/Audio.hpp>
 
 using namespace sf;
 
-namespace NESStudio
-{
+namespace NESStudio {
 
 class Frame;
 
-class Studio: public wxApp
-{
+class Studio:public wxApp {
 private:
-    Frame * frame;
-    SoundBuffer buffer;
-    Int16 * samples;
+	Frame * frame;
+	SoundBuffer buffer;
+	Int16 *samples;
 
-    virtual bool OnInit();
-    
+	virtual bool OnInit();
+
 public:
-	void Play(wxCommandEvent& event);
+	void Play(wxCommandEvent & event);
 };
 
-class Frame: public wxFrame
-{
+class Frame:public wxFrame {
 private:
 	wxButton * play;
 
 public:
 
-    Frame(const wxString& title, const wxPoint& pos, const wxSize& size);
+	Frame(const wxString & title, const wxPoint & pos,
+	      const wxSize & size);
 
-    void OnQuit(wxCommandEvent& event);
-    void OnAbout(wxCommandEvent& event);
-    void OnSave(wxCommandEvent& event);
-    void OnLoad(wxCommandEvent& event);
-    
+	void OnQuit(wxCommandEvent & event);
+	void OnAbout(wxCommandEvent & event);
+	void OnSave(wxCommandEvent & event);
+	void OnLoad(wxCommandEvent & event);
 
-
-    DECLARE_EVENT_TABLE()
+	 DECLARE_EVENT_TABLE()
 };
 
-enum
-{
-    ID_Quit = 1,
-    ID_About,
-    ID_Save,
-    ID_Load,
-    BUTTON_Play,
+enum {
+	ID_Quit = 1,
+	ID_About,
+	ID_Save,
+	ID_Load,
+	BUTTON_Play,
 };
 }
